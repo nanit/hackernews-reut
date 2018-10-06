1540c8f6b54c01deabcbf87a2af822b438eb77d0
Reut Sharabani <reut.sharabani@gmail.com>
2018-10-06 15:09:49 +0300

    added get data endpoints and tests

--- app/src/app/handler.clj
+++ app/src/app/handler.clj
@@ -6,9 +6,11 @@
             [app.db.api :as db-api]
             ;; no CSRF token in api-defaults. consider adding site-defaults
 
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [app.db.api :as db]))
 
-
+(defn parse-int [s]
+  (Integer. s))
 
 (defn top-posts [req]
   "top posts")
@@ -20,10 +22,16 @@
         text (get-in req [:json-params "text"])]
     (response (db-api/add-post author-id text))))
 
+(defn get-post [post-id]
+  (response (db-api/get-post post-id)))
+
 
 ;; users
 (defn add-user [req]
   (response (db-api/add-user (get-in req [:json-params "name"]))))
+
+(defn get-user [id]
+  (response (db-api/get-user id)))
 
 
 ;; votes
@@ -32,20 +40,27 @@
         post-id (get-in req [:json-params "post-id"])]
     (response (db-api/add-vote voter-id post-id))))
 
+(defn get-vote [id]
+  (response (db-api/get-vote id)))
+
+
 
 (defroutes post-routes
   (context "/posts" []
            (PUT "/" req (add-post req))
+           (GET "/:post-id" [post-id] (get-post (parse-int post-id)))
            (route/not-found "Not Found")))
 
 (defroutes user-routes
   (context "/users" []
            (PUT "/" req (add-user req))
+           (GET "/:id" [id] (get-user (parse-int id)))
            (route/not-found "Not Found")))
 
 (defroutes vote-routes
   (context "/votes" []
            (PUT "/" req (add-vote req))
+           (GET "/:id" [id] (get-vote (parse-int id)))
            (route/not-found "Not Found")))
 
 (defroutes api
--- app/test/app/handler_test.clj
+++ app/test/app/handler_test.clj
@@ -5,34 +5,90 @@
             [clojure.data.json :as json]
             [app.handler :refer :all]))
 
-;;(-> (mock/request :put "/users") (mock/json-body {:name "username"}))
+
+;; users
+(defn create-user [user-name]
+  (let [response (app (->
+                       (mock/request :put "/users")
+                       (mock/json-body {:name user-name})))]
+    (json/read-str
+     (:body response))))
+
+(defn get-user-by-id [id]
+  (let [response (app (mock/request :get (str "/users/" (str id))))]
+    (json/read-str
+     (:body response))))
+
+
+;; posts
+(defn create-post [user-id post-text]
+  (let [response (app (->
+                       (mock/request :put "/posts")
+                       (mock/json-body {:author-id user-id :text post-text})))]
+    (json/read-str
+     (:body response))))
+
+(defn get-post-by-id [id]
+  (let [response (app (mock/request :get (str "/posts/" (str id))))]
+    (json/read-str
+     (:body response))))
+
+
+
+;; votes
+(defn create-vote [user-id, post-id]
+  (let [response (app (->
+                       (mock/request :put "/votes")
+                       (mock/json-body {:voter-id user-id :post-id post-id})))]
+    (json/read-str
+     (:body response))))
+
+(defn get-vote-by-id [id]
+  (let [response (app (mock/request :get (str "/votes/" (str id))))]
+    (json/read-str
+     (:body response))))
+
+
 
 (deftest test-api
-  (testing "users"
+  (testing "test all endpoints sanity"
     (db/delete-db)
-    (let [username "reut"
-          create-user-request (->
-                               (mock/request :put "/users")
-                               (mock/json-body {:name username}))
-          create-user-response (app create-user-request)]
-      (is (= 200 (:status create-user-response)))
-      (let [user (json/read-str (:body create-user-response))]
-        (is (= username (get user "name")))
-        (let [user-id (get user "id")
-              post-text "text text text"
-              create-post-request (->
-                                   (mock/request :put "/posts")
-                                   (mock/json-body {:author-id user-id :text post-text}))
-              create-post-response (app create-post-request)]
-          (is (= 200 (:status create-post-response)))
-          (let [post (json/read-str (:body create-post-response))]
-            (is (= post-text (get post "text")))
-            (let [post-id (get post "id")
-                  create-vote-request (->
-                                       (mock/request :put "/votes")
-                                       (mock/json-body {:voter-id user-id :post-id post-id}))
-                  create-vote-response (app create-vote-request)]
-              (is (= 200 (:status create-vote-response)))
-              (let [vote (json/read-str (:body create-vote-response))]
-                (is (= user-id (get vote "voter_id")))
-                (is (= post-id (get vote "post_id")))))))))))
+    (let [user-name "reut"
+          user (create-user user-name)]
+      (is (= user-name (get user "name")))
+      (let [user-id (get user "id")
+            post-text "text text text"
+            post (create-post user-id post-text)]
+        (is (= post-text (get post "text")))
+        (let [post-id (get post "id")
+              vote (create-vote user-id post-id)]
+          (is (= user-id (get vote "voter_id")))
+          (is (= post-id (get vote "post_id")))))))
+
+
+  (testing "get data"
+    (db/delete-db)
+    (let [user-name "reut"
+          post-text "text text text"
+          user (create-user user-name)
+          user-id (get user "id")
+          post (create-post user-id post-text)
+          post-id (get post "id")
+          vote (create-vote user-id post-id)
+          vote-id (get vote "id")]
+      (let [generated-user (get-user-by-id user-id)
+            generated-post (get-post-by-id post-id)
+            generated-vote (get-vote-by-id vote-id)]
+
+      ;;user
+      (is (= user-id (get generated-user "id")))
+      (is (= user-name (get generated-user "name")))
+
+      ;; post
+      (is (= post-id (get generated-post "id")))
+      (is (= user-id (get generated-post "author_id")))
+
+      ;; vote
+      (is (= vote-id (get generated-vote "id")))
+      (is (= user-id (get generated-vote "voter_id")))
+      (is (= post-id (get generated-vote "post_id")))))))
